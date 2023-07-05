57a143ae78c8ff54ed80f253fb3a39ceb19266c1
loldruger <loldruger@gmail.com>
2023-07-05 14:09:26 +0000

    Change name order state to order status

--- src/response/mod.rs
+++ src/response/mod.rs
@@ -1,11 +1,11 @@
 mod accounts_info;
 mod order_info;
 mod order_chance;
-mod order_state;
+mod order_status;
 mod response_error;
 
 pub use accounts_info::*;
 pub use order_info::*;
 pub use order_chance::*;
-pub use order_state::*;
+pub use order_status::*;
 pub use response_error::*;
