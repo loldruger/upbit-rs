8d42e0c7d6ac26745824daebce08b8927ffbc98c
loldruger <loldruger@gmail.com>
2023-07-05 14:19:33 +0000

    Fix avg_buy_price value

--- src/response_source/accounts_info.rs
+++ src/response_source/accounts_info.rs
@@ -2,19 +2,19 @@ use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
 pub struct AccountsInfoSource {
-    pub currency: String,
-    pub balance: String,
-    pub locked: String,
-    pub avg_buy_price: String,
-    pub avg_buy_price_modified: bool,
-    pub unit_currency: String,
+    currency: String,
+    balance: String,
+    locked: String,
+    avg_buy_price: String,
+    avg_buy_price_modified: bool,
+    unit_currency: String,
 }
 
 impl AccountsInfoSource {
     pub fn currency(&self) -> String { self.currency.to_owned() }
     pub fn balance(&self) -> f64 { self.balance.parse().unwrap() }
     pub fn locked(&self) -> f64 { self.locked.parse().unwrap() }
-    pub fn avg_buy_price(&self) -> f64 { self.balance.parse().unwrap() }
+    pub fn avg_buy_price(&self) -> f64 { self.avg_buy_price.parse().unwrap() }
     pub fn avg_buy_price_modified(&self) -> bool { self.avg_buy_price_modified }
     pub fn unit_currency(&self) -> String { self.unit_currency.to_owned() }
 }
