16bb9889d2fd6e04aff7602c6e4b2b9eb7ca09d9
loldruger <loldruger@gmail.com>
2023-07-07 08:27:32 +0000

    Revise docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,22 @@
+/// Module for exchange APIs
 pub mod api_exchange;
+/// Module for quotation APIs
 pub mod api_quotation;
+/// Module for withdrawal APIs
 pub mod api_withdraw;
+/// Set of constants
 pub mod constant;
+/// Set of concrete request bodies
 pub mod request;
+/// Set of concrete response bodies
 pub mod response;
 
-/// set secret key
+/// function for setting secret key
 pub fn set_secret_key(secret_key: &str) {
     envmnt::set("SECRET_KEY", secret_key);
 }
 
-/// set access_key
+/// function for setting access_key
 pub fn set_access_key(access_key: &str) {
     envmnt::set("ACCESS_KEY", access_key);
 }
